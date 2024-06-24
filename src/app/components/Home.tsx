ed088539385294b15efd2e02a59c5b70ccb3c8ca
cryptolisboa <dey00tsnetwork@gmail.com>
2024-06-24 23:52:12 +0100

    refactor: update Home component to display non-default and default roles names

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,8 +1,9 @@
-"use client";
-import React from "react";
-import { signIn, signOut } from "next-auth/react";
-import { Session } from "next-auth";
-import { GuildMember } from "@/services/discord/types";
+'use client';
+import React from 'react';
+import { signIn, signOut } from 'next-auth/react';
+import { Session } from 'next-auth';
+import { GuildMember } from '@/services/discord/types';
+import { MONAD_DEFAULT_ROLE_NAME, MONAD_ROLES } from '@/constants/monad_roles';
 
 export default function HomeComponent({
   session,
@@ -14,6 +15,17 @@ export default function HomeComponent({
   userGuildMember: GuildMember | undefined;
 }) {
   const roles = userGuildMember?.roles;
+  let roleNames = roles?.map((role) => {
+    const potentialMonadRole = MONAD_ROLES[role as keyof typeof MONAD_ROLES];
+    return potentialMonadRole ? potentialMonadRole : MONAD_DEFAULT_ROLE_NAME;
+  });
+  // if there's at least one role that's not the default role, we want to only show the roles that are not the default role
+  const hasNonDefaultRole = roleNames?.some((role) => role !== MONAD_DEFAULT_ROLE_NAME);
+  if (hasNonDefaultRole) {
+    roleNames = roleNames?.filter((role) => role !== MONAD_DEFAULT_ROLE_NAME);
+  } else {
+    roleNames = [MONAD_DEFAULT_ROLE_NAME];
+  }
   return (
     <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex lg:flex-col">
       {session ? (
@@ -22,13 +34,13 @@ export default function HomeComponent({
           <button onClick={() => signOut()}>Sign out</button>
           <h2>
             {userIsPartOfMonad
-              ? "You are part of Monad"
-              : "You are not part of Monad yet, please join the discord"}
+              ? 'You are part of Monad'
+              : 'You are not part of Monad yet, please join the discord'}
           </h2>
           {userIsPartOfMonad && (
             <>
               <h3>These are your roles:</h3>
-              <div>{roles?.map((role) => <div key={role}>{role}</div>)}</div>
+              <div>{roleNames?.map((roleName) => <div key={roleName}>{roleName}</div>)}</div>
             </>
           )}
         </>
---
+++ src/constants/monad_roles.ts
@@ -0,0 +1,17 @@
+export const MONAD_ROLES = {
+  '1072682201658970112': 'full_access',
+  '1164939877650022443': 'builder_dev',
+  '1194343129147519026': 'builder_community',
+  '1194342915598721105': 'builder_artdesign',
+  '1194338643091935333': 'builder_product',
+  '1194003002298740816': 'local_nads',
+  '1051562453495971941': 'nads',
+  '1151563303827554334': 'monartist',
+  '1038728318666682378': 'quant',
+  '1046330093569593418': 'nad-og',
+  '1085044350939050075': 'running_hot',
+  '1037873237159321612': 'mon',
+  '1172573469108613130': 'mon2',
+};
+
+export const MONAD_DEFAULT_ROLE_NAME = 'pleb';
